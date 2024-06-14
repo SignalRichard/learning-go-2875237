6cc259adef711542bd9169cd1dd7ca96294d0c5c
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2024-06-14 10:51:24 -0400

    feat: 02-08b

--- practice/main.go
+++ practice/main.go
@@ -1,20 +1,32 @@
+// Write your answer here, and then test your code.
+// Your job is to implement the findLargest() method.
+
 package main
 
 import (
-	"fmt"
-	"time"
+	"strconv"
 )
 
-func main() {
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
 
-	n := time.Now()
+// calculate() returns the sum of the two parameters
+func calculate(value1 string, value2 string) float64 {
+	// Your code goes here.
 
-	fmt.Println("I recorded this video at ", n)
+	// Convert the first string to a float64
+	f1, err1 := strconv.ParseFloat(value1, 64)
+	if err1 != nil {
+		f1 = 0
+	}
+	// Convert the second string to a float64
+	f2, err2 := strconv.ParseFloat(value2, 64)
+	if err2 != nil {
+		f2 = 0
+	}
+	// Calculate and return the result
 
-	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
-	fmt.Println("Go launched at ", t)
-	fmt.Println(t.Format(time.ANSIC))
-
-	parsedTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 23:00:00 2009")
-	fmt.Printf("The type of parsedTime is %T\n", parsedTime)
+	return f1 + f2
 }
