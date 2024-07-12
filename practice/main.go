fce3639b09159b3a56a95177a702bee274ac2981
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2024-07-12 10:26:44 -0400

    feat: 03_07b

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+// Write your answer here, and then test your code.
 
-func main() {
-	fmt.Println("Hello from Go!")
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
+
+// Converts a slice of strings to a map object.
+func convertToMap(items []string) map[string]float64 {
+
+	// Create a map object
+	result := make(map[string]float64)
+
+	price := float64(100 / len(items))
+
+	for k := range items {
+		result[items[k]] = price
+	}
+
+	return result
 }
