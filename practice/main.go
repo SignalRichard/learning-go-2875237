4745bbda3237a60ad14c9898087d8b0d56d8b679
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2024-07-12 12:11:12 -0400

    feat: 04_04e

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,26 @@
+// Write your answer here, and then test your code.
+
 package main
 
-import (
-	"fmt"
-)
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
 
-func main() {
-	fmt.Println("Hello from Go!")
+type cartItem struct {
+	name     string
+	price    float64
+	quantity int
+}
+
+// calculateTotal() returns the total value of the shopping cart.
+func calculateTotal(cart []cartItem) float64 {
+
+	var total float64 = 0
+
+	for _, item := range cart {
+		total += (item.price * float64(item.quantity))
+	}
+
+	return total
 }
