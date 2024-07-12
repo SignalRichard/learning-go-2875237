b912a38ab11f089bf18199d4814b8860e97c323b
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2024-07-12 13:24:35 -0400

    feat: init

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,34 @@
+// Write your answer here, and then test your code.
+
 package main
 
-import (
-	"fmt"
-)
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
 
-func main() {
-	fmt.Println("Hello from Go!")
+// calculate() returns the result of the requested operation.
+func calculate(input1 string, input2 string, operation string) float64 {
+	// Your code goes here.
+	return 0
+}
+
+func convertInputToValue(input string) float64 {
+	return 0
+}
+
+func addValues(value1, value2 float64) float64 {
+	return 0
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return 0
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return 0
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return 0
 }
